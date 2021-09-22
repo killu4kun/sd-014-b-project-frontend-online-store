1f6437e43d62d2965575f92415dbe79dc221e0a1
Lombeira <glomenha@gmail.com>
2021-09-22 12:06:07 -0300

    Refatoração Geral Matheus e Glauco

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './components/Content';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Content />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/shoppingcart" component={ ShoppingCart } />
+        <Route path="/products/:id/:name" component={ Products } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ProductCatalog.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Card from './Card';
+
+class ProductCatalog extends React.Component {
+  render() {
+    const { productCatalog } = this.props;
+    return (
+      productCatalog.map(({ title, thumbnail, price, id }) => (
+        <Link
+          data-testid="product-detail-link"
+          key={ id }
+          to={ `/products/${id}/${title}` }
+        >
+          <Card
+            title={ title }
+            thumbnail={ thumbnail }
+            price={ price }
+          />
+        </Link>
+      ))
+    );
+  }
+}
+
+ProductCatalog.propTypes = {
+  productCatalog: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+export default ProductCatalog;
